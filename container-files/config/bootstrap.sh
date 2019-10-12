f0b209c0595035e1f1e1ad0b0b4230cde73dc81f
nopost <nopost@qq.com>
2019-10-12 13:38:27 +0800

    add proxy

---
+++ container-files/config/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+if [ "$(ls /config/init/)" ]; then
+  for init in /config/init/*.sh; do
+    chmod +x . $init \
+    && . $init
+  done
+fi
+
+if [[ $@ ]]; then 
+  eval $@
+else 
+  export PS1='[\u@\h : \w]\$ ' && \
+  /bin/bash
+fi
